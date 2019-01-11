65aa70e89b96b002689aa73afcc5658f65564b39
Guilherme Stain <guilhermestain@gmail.com>
2019-01-11 08:57:35 -0500

    atualizacao

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,14 @@
-function divisaoMaster(numero1,numero2,numero3){
+//function divisao(numero1,numero2,numero3){
+//  return (numero1,numero2/numero3);
+//}
+//console.log(divisao(2,2,0))
+
+function divisaoMaster(numero1, numero2, numero3){
+if (numero3===0){
+    return "Coloque um numero sem ser o zero";
+}else {
     return (numero1,numero2/numero3);
 }
-console.log(divisaoMaster(2,2,0))
+
+}
+console.log(divisaoMaster(2,2,2))
